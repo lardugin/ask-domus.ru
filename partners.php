427abe9a2c12897575a53cc7f94fb6c8ec3027b4
Михаил Лардугин <mihail.lardugin@gmail.com>
2017-03-04 15:55:55 +0300

    add suppliers and partners pages

---
+++ partners.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta id="myViewport" name="viewport" content="width=750">
+	<meta charset="utf-8">
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+	<meta name="skype_toolbar" content="skype_toolbar_parser_compatible">
+	<title>Наши партнеры</title>
+	<?php include "lessComp/compact.php"; ?>
+	<!-- <link rel="stylesheet" type="text/css" href="css/style.css"> -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700&subset=cyrillic" rel="stylesheet">
+	<!--[if IE 8]>
+		<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+<body>
+
+<?php include "./header.php"; ?>
+
+<div class="content-box">
+	<section class="container">
+		<div class="breadcrumbs">
+			<div class="breadcrumbs__item" itemscope="" itemtype="http://data-vocabulary.org/Breadcrumb">
+				<a href="/" itemprop="url"><span itemprop="title">Главная</span></a>
+			</div>
+			<div class="breadcrumbs__item" itemscope="" itemtype="http://data-vocabulary.org/Breadcrumb">
+				<a href="/" itemprop="url"><span itemprop="title">Сотрудничество</span></a>
+			</div>
+			<div class="breadcrumbs__item" itemscope="" itemtype="http://data-vocabulary.org/Breadcrumb">
+				<span itemprop="title">Наши партнеры</span>
+			</div>
+		</div>
+
+		<h1 class="heading">Наши партнеры</h1>
+
+		<div class="partners-page">
+			<div class="partners-list">
+				<div class="partners-list__item">
+					<div class="partners-item__img">
+						<img src="images/partners/logo_aksioma.png" alt="axiomastudio.ru">
+					</div>
+					<div class="partners-item__text">
+						<div class="content">
+							<p>Разработка концепции, световое оформление интерьеров и архитектурно-художественная подсветка – основное направление в деятельности компании. Нам одинаково интересны проекты освещения разных типов и назначения. Частные интерьеры, офисные и торговые помещения, садово-парковые зоны и архитектурно-художественная подсветка зданий. Все это попадает в сферу интересов компании. </p>
+						</div>
+						<div class="partners-item__link">
+							<a href="http://axiomastudio.ru" rel="nofollow" target="_blank">axiomastudio.ru</a>
+						</div>
+					</div>
+				</div>
+				<div class="partners-list__item">
+					<div class="partners-item__img">
+						<img src="http://ask-domus.ru/upload/medialibrary/f71/logo_FormulaDesign.png" alt="">
+					</div>
+					<div class="partners-item__text">
+						<div class="content">
+							<p>
+
+							</p>
+						</div>
+						<div class="partners-item__link">
+							<a href="http://" rel="nofollow" target="_blank"></a>
+						</div>
+					</div>
+				</div>
+				<div class="partners-list__item">
+					<div class="partners-item__img">
+						<img src="images/partners//interologo.jpg" alt="interodesign.ru">
+					</div>
+					<div class="partners-item__text">
+						<div class="content">
+							<p>
+								Компания «INTERO design» занимается поставками мебели и предметов интерьера. Мебельный дизайн и элементы интерьера для вашей квартиры.
+							</p>
+						</div>
+						<div class="partners-item__link">
+							<a href="http://interodesign.ru" rel="nofollow" target="_blank">interodesign.ru</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="content">
+			<p>В т.ч. выдачу ЭП, подбор закупок, анализ аукционной и конкурсной документации, подготовку первых и вторых частей заявок, оказываем юридическое сопровождение и предоставляем интересы в органах надзора, а также содействуем в финансовом обеспечении на всех этапах.</p>
+			<p>Уважаемые коллеги, обращаем ваше внимание, что мы зафиксировали ряд компаний и недобросовестных конкурентов, которые представляются сотрудниками нашей <i>компании и предлагают "серые" банковские гарантии!</i></p>
+			<p> Подготовить рабочее место может практически любой опытный системный администратор. Компании, не имеющие в штате подобного сотрудника, вынуждены прибегать к сторонней помощи.</p>
+		</div>
+	</section>
+</div>
+
+<?php include "./footer.php"; ?>
+
+</body>
+</html>
---
+++ suppliers.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta id="myViewport" name="viewport" content="width=750">
+	<meta charset="utf-8">
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+	<meta name="skype_toolbar" content="skype_toolbar_parser_compatible">
+	<title>Поставщикам</title>
+	<?php include "lessComp/compact.php"; ?>
+	<!-- <link rel="stylesheet" type="text/css" href="css/style.css"> -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700&subset=cyrillic" rel="stylesheet">
+	<!--[if IE 8]>
+		<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+<body>
+
+<?php include "./header.php"; ?>
+
+<div class="content-box">
+	<section class="container">
+		<div class="breadcrumbs">
+			<div class="breadcrumbs__item" itemscope="" itemtype="http://data-vocabulary.org/Breadcrumb">
+				<a href="/" itemprop="url"><span itemprop="title">Главная</span></a>
+			</div>
+			<div class="breadcrumbs__item" itemscope="" itemtype="http://data-vocabulary.org/Breadcrumb">
+				<a href="/" itemprop="url"><span itemprop="title">Сотрудничество</span></a>
+			</div>
+			<div class="breadcrumbs__item" itemscope="" itemtype="http://data-vocabulary.org/Breadcrumb">
+				<span itemprop="title">Поставщикам</span>
+			</div>
+		</div>
+
+		<h1 class="heading">Поставщикам</h1>
+
+		<div class="content">
+			<p>
+				Уважаемые поставщики мебели и отделочных материалов, представители строительных компаний, предметов интерьера и других объектов!<br>
+				Если Вы заинтересованы в сотрудничестве с нами, пожалуйста, пишите нам на e-mail: <a href="mailto:info@ask-domus.ru">info@ask-domus.ru</a>.<br>
+				Обязательно ставьте в теме пометку <strong>"Сотрудничество"</strong> и указывайте в теме сферу деятельности вашей компании.
+			</p>
+			<p>К сожалению, мы не имеем возможности рассматривать коммерческие предложения по телефону. Пожалуйста, уважайте свое и наше время!</p>
+		</div>
+	</section>
+</div>
+
+<?php include "./footer.php"; ?>
+
+</body>
+</html>
